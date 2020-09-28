1061bf5e96a66c2b02ecc2fb23739e3a29668b9b
chris <christopher.jan.oosthuizen@gmail.com>
2020-09-27 19:11:25 -0500

    added

---
+++ FakeSql/Server/ServSock.cpp
@@ -0,0 +1,5 @@
+//
+// Created by Chris on 9/27/2020.
+//
+
+#include "ServSock.h"
---
+++ FakeSql/Server/ServSock.h
@@ -0,0 +1,14 @@
+//
+// Created by Chris on 9/27/2020.
+//
+
+#ifndef FAKESQL_SERVSOCK_H
+#define FAKESQL_SERVSOCK_H
+
+
+class ServSock {
+
+};
+
+
+#endif //FAKESQL_SERVSOCK_H
